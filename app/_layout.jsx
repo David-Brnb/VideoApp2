0f54db958339511818e60efc462d04f650b224d9
David-Brnb <321171197@ciencias.unam.mx>
2025-01-21 10:43:46 -0600

    add: functional expo route and tailwind app

---
+++ app/_layout.jsx
@@ -0,0 +1,14 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+import "../global.css";
+
+export default function App() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className='text-3xl'>Aora!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
