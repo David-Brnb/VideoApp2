fcbc77b12bc5334726a08b88ebc536e8f5957b93
David-Brnb <321171197@ciencias.unam.mx>
2025-01-21 11:07:32 -0600

    add: this app is functional and now have fonts

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,14 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { SplashScreen, Stack } from 'expo-router'
 import "../global.css";
+import { useFonts } from 'expo-font'
+import { useEffect } from 'react';
 
-export default function App() {
-  return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className='text-3xl'>Aora!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+SplashScreen.preventAutoHideAsync();
+
+const RootLayout = () => {
+    const [fontsLoaded, error] = useFonts({
+        "Poppins-Black": require("../assets/fonts/Poppins-Black.ttf"),
+        "Poppins-Bold": require("../assets/fonts/Poppins-Bold.ttf"),
+        "Poppins-ExtraBold": require("../assets/fonts/Poppins-ExtraBold.ttf"),
+        "Poppins-ExtraLight": require("../assets/fonts/Poppins-ExtraLight.ttf"),
+        "Poppins-Light": require("../assets/fonts/Poppins-Light.ttf"),
+        "Poppins-Medium": require("../assets/fonts/Poppins-Medium.ttf"),
+        "Poppins-Regular": require("../assets/fonts/Poppins-Regular.ttf"),
+        "Poppins-SemiBold": require("../assets/fonts/Poppins-SemiBold.ttf"),
+        "Poppins-Thin": require("../assets/fonts/Poppins-Thin.ttf"),
+    });
+
+    useEffect(() => {
+        if(error) throw error;
+
+        if(fontsLoaded) SplashScreen.hideAsync();
+
+    }, [fontsLoaded, error])
+
+    if(!fontsLoaded && !error) return null;
+
+    return (
+        <Stack>
+            <Stack.Screen name="index" options={{ headerShown: false}}/>
+        </Stack>
+    )
 }
 
+export default RootLayout
