a8ad606aae3cf5fd9d447603aff42a0d30762ff4
David-Brnb <321171197@ciencias.unam.mx>
2025-01-25 10:01:04 -0600

    add: functional home page

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,16 +1,27 @@
-import { View, Text, FlatList, Image } from 'react-native'
-import React from 'react'
+import { View, Text, FlatList, Image, RefreshControl } from 'react-native'
+import React, { useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 
 import { images } from '../../constants'
 import SearchInput from '../../components/SearchInput'
 import Trending from '../../components/Trending'
+import EmptyState from '../../components/EmptyState'
 
 const Home = () => {
+  const [refreshing, setRefreshing] = useState(false)
+
+  const onRefresh = async () => {
+    setRefreshing(true);
+    console.log("refreshing")
+    //re call a video -> if any new videos appear
+    setRefreshing(false);
+  }
+
   return (
-    <SafeAreaView className="bg-primary">
+    <SafeAreaView className="bg-primary h-full">
       <FlatList 
         data={[{ id:1 }, { id:2 }, { id:3 }]}
+        // data={[/]}
         keyExtractor={(item) => item.$id}
         renderItem={({item}) => (
           <Text className="text-3xl text-white"> {item.id} </Text>
@@ -43,11 +54,20 @@ const Home = () => {
                 Latest Videos  
               </Text> 
 
-              <Trending />
+              <Trending 
+                posts={[{ id: 1}, { id: 2}, { id: 3}] ?? []}
+              />
 
             </View>
           </View>
         )}
+        ListEmptyComponent={() => (
+          <EmptyState 
+            title="No videos found"
+            subtitle="Be the first one to upload a video"
+          />
+        )}
+        refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh}/>}
       />
     </SafeAreaView>
   )
