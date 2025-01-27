53ecab64d13cb50fea14a13c702c5ee78724c85f
David-Brnb <321171197@ciencias.unam.mx>
2025-01-26 21:51:25 -0600

    add: update video

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -7,12 +7,12 @@ import SearchInput from '../../components/SearchInput'
 import VideoCard from '../../components/VideoCard'
 import Trending from '../../components/Trending'
 import EmptyState from '../../components/EmptyState'
-import { getAllPosts } from '../../lib/appwrite'
+import { getAllPosts, getLatestPosts } from '../../lib/appwrite'
 import useAppwrite from '../../lib/useAppwrite'
 
 const Home = () => {
   const { data: posts, refetch } = useAppwrite(getAllPosts);
-  console.log(posts)
+  const { data: latestPosts } = useAppwrite(getLatestPosts);
 
   const [refreshing, setRefreshing] = useState(false)
 
@@ -59,7 +59,7 @@ const Home = () => {
               </Text> 
 
               <Trending 
-                posts={[{ id: 1}, { id: 2}, { id: 3}] ?? []}
+                posts={latestPosts}
               />
 
             </View>
